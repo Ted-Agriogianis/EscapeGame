47d41f7e3c761ade3ff54fc8a7249bd3c005a614
Ted <theBuilder10000@gmail.com>
2019-07-05 16:05:17 -0400

    Added Game

---
+++ Escape Game Unreal/EscapeGame/Intermediate/Build/Mac/UE4Editor/Inc/EscapeGame/EscapeGameGameModeBase.gen.cpp
@@ -0,0 +1,86 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "EscapeGame/EscapeGameGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeEscapeGameGameModeBase() {}
+// Cross Module References
+	ESCAPEGAME_API UClass* Z_Construct_UClass_AEscapeGameGameModeBase_NoRegister();
+	ESCAPEGAME_API UClass* Z_Construct_UClass_AEscapeGameGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_EscapeGame();
+// End Cross Module References
+	void AEscapeGameGameModeBase::StaticRegisterNativesAEscapeGameGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_AEscapeGameGameModeBase_NoRegister()
+	{
+		return AEscapeGameGameModeBase::StaticClass();
+	}
+	struct Z_Construct_UClass_AEscapeGameGameModeBase_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AEscapeGameGameModeBase_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_EscapeGame,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AEscapeGameGameModeBase_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "EscapeGameGameModeBase.h" },
+		{ "ModuleRelativePath", "EscapeGameGameModeBase.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AEscapeGameGameModeBase_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AEscapeGameGameModeBase>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AEscapeGameGameModeBase_Statics::ClassParams = {
+		&AEscapeGameGameModeBase::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009002A8u,
+		METADATA_PARAMS(Z_Construct_UClass_AEscapeGameGameModeBase_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_AEscapeGameGameModeBase_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AEscapeGameGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AEscapeGameGameModeBase_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AEscapeGameGameModeBase, 1671258295);
+	template<> ESCAPEGAME_API UClass* StaticClass<AEscapeGameGameModeBase>()
+	{
+		return AEscapeGameGameModeBase::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AEscapeGameGameModeBase(Z_Construct_UClass_AEscapeGameGameModeBase, &AEscapeGameGameModeBase::StaticClass, TEXT("/Script/EscapeGame"), TEXT("AEscapeGameGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AEscapeGameGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
